7781b584ff9257e116c11f35f15545a001f85874
Phalor <niguinti@e1r2p19.42.fr>
2019-10-22 21:08:01 +0200

    get next token

--- src/main.c
+++ src/main.c
@@ -9,15 +9,17 @@ int main(int ac, char **av)
 {
 	char *input = av[1];
 	t_flags	f;
+	t_token	tok;
 
 	if (ac < 2)
 	{
-		printf("Usage: ./rdp \"2*8+(1 - 2)\"\n\t\t\t-debug=1\n\t\t\t-ast=draw\n");
+		printf("Usage: ./rdp \"2*8+(1 - 2)\" [-debug=all] [-ast=draw]\n");
 		return (0);
 	}
 	f = check_param(av + 2);
 	if (f.debug_all)
 		printf("f.d = %u\nf.a = %u\n", f.debug_all, f.ast_draw);
+	get_next_token(input);
 	factor(input);
 	return 0;
 }
---
+++ src/tokenizer.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokenizer.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: niguinti <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/01 05:04:06 by niguinti          #+#    #+#             */
+/*   Updated: 2019/10/22 21:07:44 by niguinti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "tokenizer.h"
+#include "tokenizer_rules.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+t_tokens	*save_token(char *s, int anchor, t_toktype toktype)
+{
+}
+
+void	ignore_wspace(char *s, int *i)
+{
+	while (s[*i] && get_chr_class[(unsigned char)s[*i]] == CHR_SPACE)
+		(*i)++;
+}
+
+t_toktype	get_operator(char *s, t_toktype toktype)
+{
+	if (!strcmp(s, "+"))
+		return (TOK_PLUS);
+	if (!strcmp(s, "-"))
+		return (TOK_MINUS);
+	if (!strcmp(s, "*"))
+		return (TOK_MUL);
+	if (!strcmp(s, "/"))
+		return (TOK_DIVIDE);
+	return (0);
+}
+
+t_tokens	*get_token(char *s, int *i, t_toktype toktype, t_chr_class prev_class)
+{
+	t_chr_class chr_class = 0;
+	int anchor = 0;
+	
+	chr_class = get_chr_class[(unsigned char)s[*i]];
+	while (s[*i] &&
+			(token_chr_rules[toktype][(chr_class = get_chr_class[(unsigned char)s[*i]])]
+				|| prev_class == CHR_ESCAPE))
+	{
+		prev_class = chr_class;
+		//printf("[%s, '%c', %d]\n", DEBUG_CHR[chr_class], s[*i], anchor);
+		anchor++;
+		(*i)++;
+	}
+	//printf("{%s, \"%.*s\"}\n", DEBUG_TOKEN[toktype], anchor, s + (*i - anchor));
+	return (save_token(s + (*i - anchor), anchor, toktype));
+}
+
+t_tokens	*get_next_token(char *s)
+{
+	_chr_class		chr_class = 0;
+	t_toktype		toktype = 0;
+	t_tokens		*token = NULL;
+	static	int		i = 0;
+
+	if (s[i] == '\0')
+		return (save_token(NULL, 0, TOK_EOF));
+	if (!(chr_class = get_chr_class[(unsigned char)s[i]]))
+		return (NULL);
+	if (chr_class == CHR_COMMENT || chr_class == CHR_SP)
+	{
+		ignore_chr_class(s, &i, chr_class);
+		return (get_next_token(s));
+	}
+	if (!(toktype = get_tok_type[chr_class]))
+		return (NULL);
+	if (is_opening_class(chr_class))
+	{
+		i++;
+		token = get_sequence_token(s, &i,  toktype, chr_class);
+		i++;
+	}
+	else
+		token = get_token(s, &i, toktype, chr_class);
+	if (ABSTRACT_TOKEN[token->tok] && !(token->tok = get_true_toktype(token->data, token->tok)))
+		return (NULL);
+	return (token);
+}
+
+int main(int argc, char *argv[])
+{
+	t_tokens	*tok;
+	
+	(void)argc;
+	tok = get_next_token(argv[1]);
+	while (tok && tok->tok != TOK_EOF)
+	{
+		printf("{%s, \"%s\"}\n", DEBUG_TOKEN[tok->tok], tok->data);
+		tok = get_next_token(argv[1]);
+	}
+	if (!tok)
+		printf("Syntax error\n");
+	return 0;
+}
