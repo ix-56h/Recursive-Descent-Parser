e7495f7cf38594024c20651bc7c3597b5ac92438
Phalor <niguinti@e1r12p12.42.fr>
2019-10-30 05:41:20 +0100

    nodes

--- src/main.c
+++ src/main.c
@@ -1,20 +1,30 @@
 #include "rdp.h"
 
+t_flags		f;
+
 t_node	*save_node(t_node *left, t_tokens tok, t_node *right)
 {
-	return ((t_node*)0);
+	t_node	*node = NULL;
+
+	if (!(node = malloc(sizeof(t_node))))
+		exit(1);
+	node->left = left;
+	node->right = right;
+	node->tok = tok.tok;
+	node->data = tok.data;
+	return (node);
 }
 
 void	factor(char *s, t_tokens *cur, t_node *node)
 {
-	printf("fac -> %s\n", DEBUG_TOK[cur->tok]);
+	(f.debug_all ? printf("factor -> %s\n", DEBUG_TOK[cur->tok]) : 0);
 	if (cur->tok == TOK_DIGIT)
 		*cur = get_next_token(s);
 	else if (cur->tok == TOK_LPAREN)
 	{
 		*cur = get_next_token(s);
 		expr(s, cur, node);
-		printf("fac2 -> %s\n", DEBUG_TOK[cur->tok]);
+		(f.debug_all ? printf("factor lparen -> %s\n", DEBUG_TOK[cur->tok]) : 0);
 		if (cur->tok == TOK_RPAREN)
 			*cur = get_next_token(s);
 		else
@@ -35,9 +45,10 @@ void	factor(char *s, t_tokens *cur, t_node *node)
 void	term(char *s, t_tokens *cur, t_node *node)
 {
 	factor(s, cur, node);
+	(f.debug_all ? printf("term -> %s\n", DEBUG_TOK[cur->tok]) : 0);
 	while (cur->tok == TOK_MUL || cur->tok == TOK_DIVIDE)
 	{
-		printf("term -> %s\n", DEBUG_TOK[cur->tok]);
+		(f.debug_all ? printf("term -> %s\n", DEBUG_TOK[cur->tok]) : 0);
 		if (cur->tok == TOK_MUL)
 			*cur = get_next_token(s);
 		else if (cur->tok == TOK_DIVIDE)
@@ -55,10 +66,10 @@ void	term(char *s, t_tokens *cur, t_node *node)
 void	expr(char *s, t_tokens *cur, t_node *node)
 {
 	term(s, cur, node);
-
+	(f.debug_all ? printf("expr -> %s\n", DEBUG_TOK[cur->tok]) : 0);
 	while (cur->tok == TOK_PLUS || cur->tok == TOK_MINUS)
 	{
-		printf("expr -> %s\n", DEBUG_TOK[cur->tok]);
+		(f.debug_all ? printf("expr -> %s\n", DEBUG_TOK[cur->tok]) : 0);
 		if (cur->tok == TOK_PLUS)
 			*cur = get_next_token(s);
 		else if (cur->tok == TOK_MINUS)
@@ -76,7 +87,6 @@ void	expr(char *s, t_tokens *cur, t_node *node)
 int main(int ac, char **av)
 {
 	char		*input = av[1];
-	t_flags		f;
 	t_tokens	tok;
 	t_node		*node;
 
