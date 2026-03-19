75aa473c3ea49718173ac645ba8ff8628eba6f1c
Zarial <39010759+ix-56h@users.noreply.github.com>
2026-03-19 15:31:15 +0100

    Fix: fix. Fix ? FIX !

--- include/rdp.h
+++ include/rdp.h
@@ -17,10 +17,12 @@ typedef	struct		s_node
 	t_toktype		tok;
 	char			*data;
 	struct s_node	*right;
+	int				key;
 }					t_node;
 
 t_flags	check_param(char **av);
-void	term(char *s, t_tokens *cur, t_node *node);
-void	factor(char *s, t_tokens *cur, t_node *node);
-void	expr(char *s, t_tokens *cur, t_node *node);
+t_node	*term(char *s, t_tokens *cur);
+t_node	*factor(char *s, t_tokens *cur);
+t_node	*expr(char *s, t_tokens *cur);
+void	bst_print_dot(t_node *tree, FILE *stream);
 #endif
